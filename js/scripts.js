522a705d05e1bcb37419564450686488a7cd18ce
Kevin Funez <kevin.funez315@gmail.com>
2021-06-05 10:45:42 -0700

    add background color

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
     const laptopInput = $("input#laptop").val();
 
     let result;
-    if (ageInput === "Y" && laptopInput === "Y") {
+    if (ageInput === "Yes", "yes", "Y", "y" && laptopInput === "Yes", "yes", "Y", "y" ) {
       result = "Javascript"
-    } else if (ageInput === "N" && sportInput === "Y") {
+    } else if (ageInput === "No", "no", "Y", "n" && sportInput === "Yes", "yes", "Y", "y" ) {
       result = "Python"  
-    } else if (laptopInput === "Y" && codeInput === "Y") {
+    } else if (laptopInput === "Yes", "yes", "Y", "y"  && codeInput === "Yes", "yes", "Y", "y" ) {
       result = "C#" 
-    } else if (mathInput === "N" && ageInput === "Y") {
+    } else if (mathInput === "No", "no", "Y", "n" && ageInput === "Yes", "yes", "Y", "y" ) {
       result = "Ruby"
-    } else if (sportInput === "N" && laptopInput === "N") {
+    } else if (sportInput === "No", "no", "Y", "n" && laptopInput === "No", "no", "Y", "n") {
       result = "Go"
     } else {
       result = "YOU'RE TOO SMART FIGURE IT OUT!"
