118222ec8666d622c790b353804a2948c204d259
Kevin Funez <kevin.funez315@gmail.com>
2021-06-11 17:29:19 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $("#language-picker").submit(function(event) {
-    const ageInput = $("input#age").val();
+    const ageInput = $("radio#age").val();
     const sportInput = $("input#sport").val();
     const mathInput = $("input#math").val();
     const codeInput = $("input#code").val();
-    const laptopInput = $("input#laptop").val();
+    const laptopInput = $("radio#laptop").val();
 
     let result;
-    if (ageInput === "Yes" || ageInput === "yes" || ageInput === "Y" || ageInput === "y" && laptopInput === "Yes" && laptopInput === "yes" || laptopInput === "Y" || laptopInput === "y" ) {
+    if (ageInput === "Yes" && laptopInput === "Yes") {
       result = "Javascript"
-    } else if (ageInput === "Yes" || ageInput === "yes" || ageInput === "Y" || ageInput === "y" && sportInput === "Yes", "yes", "Y", "y" ) {
-      result = "Python"  
-    } else if (laptopInput === "Yes" && laptopInput === "yes" || laptopInput === "Y" || laptopInput === "y"  &&  codeInput === "Yes" || codeInput === "yes" || codeInput === "Y" || codeInput === "y" ) {
+    } else if (ageInput === "Yes" && sportInput === "Yes") {
+      result = "Python";  
+    } else if (laptopInput === "Yes" && codeInput === "Yes") {
       result = "C#" 
-    } else if (mathInput === "No" || mathInput === "no" || mathInput === "N"  || mathInput === "n" && ageInput === "Yes" || ageInput === "yes" || ageInput === "Y" || ageInput === "y" ) {
+    } else if (mathInput === "No" && ageInput === "Yes") {
       result = "Ruby"
-    } else if (sportInput === "No" || sportInput === "no" || sportInput === "N" || sportInput === "n" && laptopInput === "Yes" && laptopInput === "yes" || laptopInput === "Y" || laptopInput === "y") {
+    } else if (sportInput === "No" && laptopInput === "Yes") {
       result = "Go"
     } else {
       result = "YOU'RE TOO SMART FIGURE IT OUT!"
