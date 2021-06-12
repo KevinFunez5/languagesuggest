769b0d6829861696d47f872359abf9b1c4dc4e99
Kevin Funez <kevin.funez315@gmail.com>
2021-06-11 21:40:10 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $("#language-picker").submit(function(event) {
-    const ageInput = $("radio#age").val();
-    const sportInput = $("input#sport").val();
-    const mathInput = $("input#math").val();
-    const codeInput = $("input#code").val();
-    const laptopInput = $("radio#laptop").val();
+    const ageInput = $("#age").val();
+    const sportInput = $("#sport").val();
+    const mathInput = $("#math").val();
+    const codeInput = $("#code").val();
+    const laptopInput = $("#laptop").val();
 
     let result;
-    if (ageInput === "Yes" && laptopInput === "Yes") {
+    if (ageInput == "Yes" && laptopInput == "Yes") {
       result = "Javascript"
-    } else if (ageInput === "Yes" && sportInput === "Yes") {
-      result = "Python";  
-    } else if (laptopInput === "Yes" && codeInput === "Yes") {
+    } else if (ageInput == "Yes" && sportInput == "Yes") {
+      result = "Python"
+    } else if (laptopInput == "Yes" && codeInput == "Yes") {
       result = "C#" 
-    } else if (mathInput === "No" && ageInput === "Yes") {
+    } else if (mathInput == "No" && ageInput == "Yes") {
       result = "Ruby"
-    } else if (sportInput === "No" && laptopInput === "Yes") {
+    } else if (sportInput == "No" && laptopInput == "Yes") {
       result = "Go"
     } else {
       result = "YOU'RE TOO SMART FIGURE IT OUT!"
